9a1c8459d35edf120b79512726fa77d82a851e64
ianm <iangregoryjames@gmail.com>
2021-11-25 11:25:27 -0500

    nav and responsive css grid layout

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* PERSONAL HOMEPAGE */
+
+console.log('Welcome to my personal homepage!')
+
+/* OFFSCREEN MENU */
+function nav(tgl = 'toggle', cls = 'nav-active') {
+    if (document.querySelector(`.${tgl}`)) {
+        document.querySelector(`.${tgl}`).addEventListener('click', 
+            () => { document.body.classList.toggle(cls) }
+        )
+    }
+} 
+nav()
